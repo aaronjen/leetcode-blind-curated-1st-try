43a686e2bed7771ff30741751243244de1c6ec3a
Aaron Jen <aaronjen@jubo.health>
2020-12-20 16:25:05 +0800

    commit missing-number

---
+++ 268.missing-number/main.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+ * @lc app=leetcode id=268 lang=golang
+ *
+ * [268] Missing Number
+ */
+
+// @lc code=start
+func missingNumber(nums []int) int {
+	n := len(nums)
+	sum := n * (n + 1) / 2
+	for i := 0; i < n; i++ {
+		sum -= nums[i]
+	}
+	return sum
+}
+
+// @lc code=end
